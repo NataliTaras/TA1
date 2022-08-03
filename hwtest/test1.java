9a8402fe8a1131243241b23cde7dd4af54ee771f
NT <nt@gmail.com>
2022-08-04 00:25:43 +0300

    hdfjfjh

---
+++ hwtest/test1.java
@@ -0,0 +1,10 @@
+package TA1.hwtest;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
+    }
+}
